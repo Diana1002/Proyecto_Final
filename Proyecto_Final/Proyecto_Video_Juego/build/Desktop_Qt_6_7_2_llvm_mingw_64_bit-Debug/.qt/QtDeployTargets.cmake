ade4116a0d0ec628e0cce014a668aa3f67bd2a17
Diana1002 <diana.baeza@udea.edu.co>
2024-11-12 13:18:20 -0500

    Creación del proyecto en Qt

---
+++ Proyecto_Final/Proyecto_Video_Juego/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Proyecto_Video_Juego_FILE C:/Users/Asus/Documents/DIANA BAEZA RUIZ/SEMESTRE 2024-2/Proyecto_Final/Proyecto_Final/Proyecto_Video_Juego/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/Proyecto_Video_Juego.exe)
+set(__QT_DEPLOY_TARGET_Proyecto_Video_Juego_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Proyecto_Video_Juego_RUNTIME_DLLS C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
